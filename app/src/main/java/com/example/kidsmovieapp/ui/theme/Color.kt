f709faeda3faf61a81e01b78792a914383d012e6
Dareen Hassan <dareenhassan2018@gmail.com>
2025-10-03 17:14:12 +0300

    Added gradient colors

--- app/src/main/java/com/example/kidsmovieapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/kidsmovieapp/ui/theme/Color.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.graphics.Color
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
 val PinkGradient = Color(0xFFFFC1E3)
 val BlueGradient = Color(0xFFB3E5FC)
 val PurpleGradient = Color(0xFFE1BEE7)
