348fe3089c64366445579c1189675a34f3aa37b6
slsbyl <slsbylahmed86@gmail.com>
2025-10-03 17:07:10 +0300

    Delete app directory

--- app/src/main/java/com/example/kidsmoviesapp/Movie.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kidsmoviesapp
-
-data class Movie(
-    val id: Int,
-    val title: String,
-    val poster_path: String?
-)
--- app/src/main/java/com/example/kidsmoviesapp/TmdbApi.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kidsmoviesapp
-
-object TmdbApi {
-    const val API_KEY = "165a1f27bba225269a0d15654811198f"
-    const val BASE_URL = "https://api.themoviedb.org/3/"
-}
