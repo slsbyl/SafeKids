36c23fd816debf7951cd9d93eadde0d44b09bb36
Basant Mohamed <your_email@example.com>
2025-10-06 01:51:20 +0300

    Adding UI-SplashScreen

--- app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.kidsmovieapp.screens.SafeKidsHomeScreen
+import com.example.kidsmovieapp.screens.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SafeKidsHomeScreen()
+            SplashScreen {  }
         }
     }
 }
