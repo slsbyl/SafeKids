f0e85f86a85a7bf4f0b22b419f3c037c0359b6bf
roaaa <roaaabdelghny39@gmail.com>
2025-10-07 00:32:38 +0300

    Restore KidsApp/MovieApp files after mistaken revert

---
+++ app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kidsmovieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.kidsmovieapp.screens.SafeKidsHomeScreen
+import com.example.kidsmovieapp.screens.SplashScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen {  }
+        }
+    }
+}
