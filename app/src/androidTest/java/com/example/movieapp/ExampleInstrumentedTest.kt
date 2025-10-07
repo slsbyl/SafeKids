778a0a81b111a4361127b955f82d43dac83a3763
Roaa Abdelghany <roaaabdelghany39@gmail.com>
2025-10-07 11:54:32 +0300

    Add ui-SearchScreen

--- app/src/androidTest/java/com/example/movieapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/movieapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.movieapp
+package com.example.kidsmovieapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.movieapp", appContext.packageName)
+        assertEquals("com.example.kidsmovieapp", appContext.packageName)
     }
 }
