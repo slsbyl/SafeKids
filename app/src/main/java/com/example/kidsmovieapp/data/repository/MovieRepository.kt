87c2efc989a662aec3a3092d4c261360e339a85c
Basant Mohamed <basantshabanmohamed@gmail.com>
2025-10-09 21:27:17 +0300

    API data handling

--- app/src/main/java/com/example/kidsmovieapp/data/repository/MovieRepository.kt
+++ app/src/main/java/com/example/kidsmovieapp/data/repository/MovieRepository.kt
@@ -1,4 +1,27 @@
 package com.example.kidsmovieapp.data.repository
 
+import com.example.kidsmovieapp.data.remote.api.NetworkModule
+import com.example.kidsmovieapp.data.remote.dto.MovieDto
+import com.example.kidsmovieapp.data.remote.dto.MovieListResponseDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class MovieRepository {
+
+    private val api = NetworkModule.api
+
+    //Fetch kids movies for home screen
+    suspend fun getKidsMovies(page: Int = 1): List<MovieDto> = withContext(Dispatchers.IO) {
+        api.getKidsMovies(page = page).results
+    }
+
+    //Fetch movie's names for search screen
+    suspend fun searchMovies(query: String): List<MovieDto> = withContext(Dispatchers.IO) {
+        api.searchMovies(query = query).results
+    }
+
+    //Fetch movie details for detail screen
+    suspend fun getMovieDetails(movieId: Int): MovieDto = withContext(Dispatchers.IO) {
+        api.getMovieDetails(movieId)
+    }
 }
