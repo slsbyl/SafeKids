1e702380a8a4ece3b90d529e2e459c0c55759993
salsabil ahmed <slsbylahmed86@gmail.com>
2025-10-10 14:41:32 +0300

    Updated TMDB connection and SafeKids UI

--- app/src/main/java/com/example/kidsmovieapp/data/remote/dto/MovieDto.kt
+++ app/src/main/java/com/example/kidsmovieapp/data/remote/dto/MovieDto.kt
@@ -1,10 +1,12 @@
 package com.example.kidsmovieapp.data.remote.dto
 
+import com.google.gson.annotations.SerializedName
 
 data class MovieDto(
     val id: Int,
     val title: String,
     val overview: String?,
     val poster_path: String?,
-    val release_date: String?
+    val release_date: String?,
+    @SerializedName("vote_average") val vote_average: Double?
 )
