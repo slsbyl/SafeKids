747cffa1054b0f5fa99e21d7bb10a9fdb9b8edd2
roaaa <roaaabdelghny39@gmail.com>
2025-10-14 10:32:15 +0300

    Add ui-SearchScreen

--- app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.kidsmovieapp.screens.SafeKidsHomeScreen
+import com.example.kidsmovieapp.screens.SearchScreen
 import com.example.kidsmovieapp.screens.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SafeKidsHomeScreen()
+            SearchScreen()
         }
     }
 }
