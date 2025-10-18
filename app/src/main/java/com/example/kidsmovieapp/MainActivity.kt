9e46c601c19cda0ceb97ef1d880b247f1c039689
Basmala Nasser <basmalanasser78@gmail.com>
2025-10-18 14:56:37 +0200

    Navigation setup

--- app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsmovieapp/MainActivity.kt
@@ -3,16 +3,28 @@ package com.example.kidsmovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.kidsmovieapp.navigation.NavigationGraph
 import com.example.kidsmovieapp.screens.HomeScreen
 import com.example.kidsmovieapp.screens.SafeKidsHomeScreen
 import com.example.kidsmovieapp.screens.SearchScreen
 import com.example.kidsmovieapp.screens.SplashScreen
+import com.example.kidsmovieapp.ui.theme.KidsMovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+
+            KidsMovieAppTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val navController = rememberNavController()
+                    NavigationGraph(navController=navController)
+                }
+                }
         }
     }
 }
