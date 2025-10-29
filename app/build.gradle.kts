298a529f17098a4ed5e45ddad49d10daa82c068b
Basant Mohamed <basantshabanmohamed@gmail.com>
2025-10-29 19:00:09 +0300

    Home screen modified

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
